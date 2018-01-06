af16ec3c9614ef3cf2176c96ae171c989e69a72d
James Lagattuta <jimmy.lagattuta@gmail.com>
2018-01-05 23:20:29 -0800

    wiring buying stocks

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,11 +1,20 @@
 class Api::V1::TransactionsController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
-	
 	def buy_stocks
 		puts "*" * 100
 		puts "params recieved"
 		puts params.inspect
+		pps = params['jsonPPS']
+		cash = params['jsonCash']
+		quantity = params['jsonQuantity']
+		symbol = params['jsonSymbol']
+		email = params['jsonEmail']
+		uid = params['jsonUID']
+		id = params['jsonID']
+		x = User.find_by(id: id)
+		puts "user"
+		puts x
 		message = {
 			content: 'Params Recieved'
 		}
